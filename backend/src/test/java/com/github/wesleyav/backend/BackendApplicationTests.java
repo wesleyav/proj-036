ca78cc80b5c988d5a0ecd446caecd06914e3e258
Wesley Araújo Vieira <wesley.contato@hotmail.com>
2024-12-20 22:47:59 -0300

    chore: adiciona configuração inicial do backend

---
+++ backend/src/test/java/com/github/wesleyav/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.wesleyav.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
